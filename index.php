dd161bbfc1b339c118c1c94c2f374fc18c3b0894
Celestino <celestinolanga11@gmail.com>
2019-06-25 19:16:36 +0100

    botao

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
         <h3>Login</h3>
         <input type="text" name="telefone" placeholder="numero de telefone">
         <input type="password" name="senha" placeholder="Senha">
-        <a href="#" class="botao" onclick="validarModalLogin()">Entrar</a>
+        <button class="botao" onclick="validarModalLogin()">Entrar</button>
     </div>
 </form>
 
