c41ba8945947f37e7930eda558b7294754abd9ce
Unknown <unknown@example.com>
2019-06-26 04:53:45 +0200

    Cadastro de Categoria

--- Interface/admin/registar_categoria.php
+++ Interface/admin/registar_categoria.php
@@ -7,9 +7,9 @@ include_once ("cabecalho/menu.php");
         <span>CAtegoria e Escalão/ Criar Categoria</span>
     </div>
 </div>
-<form method="post" action="Perfil_de_Oferta.php" id="formulario">
+<form method="post" action="/../controlador/Categoria.php" id="formulario">
 
-    <input class="Cat" placeholder="Informe a Categória a Registar " type="text"/>
+    <input class="Cat" placeholder="Informe a Categória a Registar " name="catego" type="text"/>
 
 
        <div class="Bottons" name="Bottons" style="float: right">
